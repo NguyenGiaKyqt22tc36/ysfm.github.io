50a5053f65a15d076967d43480ceb895227c1513
NguyenGiaKyqt22tc36 <nguyengiakyqt22tc36@gmail.com>
2024-11-05 13:31:48 +0700

    Add files via upload

---
+++ web.php
@@ -0,0 +1,48 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+<link href="kh.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+	<header>
+		
+		<div class="logo"><img src="3.png" alt="Logo" width="100">
+		</div>
+		<nav class="menu">
+		<ul>
+			<li><a href="#">HOME</a></li>
+			<li><a href="#book">BOOKS</a></li>
+			<li><a href="gopy.php">GÓP Ý</a></li></ul>
+		</nav>
+	</header>
+	<div class="cont">
+	<div class="col1">
+		<div class="search-bar">
+		<form action="/search" method="get">
+		<input type="text" name="query" placeholder="Tìm kiếm...">
+		<button type="submit">Tìm kiếm</button></form>
+			</div>
+	
+				<h2>Đề xuất</h2>
+<div class="post">
+	<img src="1.jpg" alt="Bài viết 1" width="50%">
+	<p>Mối tình ấy giờ đây khi nhớ lại khiến lá thư đầy ứ những hoài niệm vừa đẹp đẽ, vừa đau buồn....</p>
+		</div>
+		<div class="post">
+		<img src="master.jpg" alt="Bài viết 2" width="50%">
+			<p>Vì dám đứng lên bảo vệ bạn, Sori bị cả lớp bắt nạt và buộc phải chuyển trường. Tuy thế, khởi đầu mới của cô bé lại không như mong đợi. Tại đây, Sori không tài nào hòa nhập được với bạn bè, và dường như cơn ác mộng cũ vẫn chưa chịu buông tha cô...</p>
+		</div>
+			</div>
+		<div class="col2">
+		<img src="1.jpg" alt="Hình ảnh bài viết" width="100%">
+		<p>Đó là ký ức của một thằng bé xa lạ tìm một mối liên hệ với đất nước mới xa lạ, mà nó gần như trở nên vô hình. Đó là những trận đánh đập từ những kẻ bắt nạt ở trường mà nó phải chịu đựng khi tìm cách hòa nhập vào đời sống ở Mỹ. Đó là những sự kiện vụn vặt dở khóc dở cười của một gia đình nhập cư chỉ có đứa con đi học và biết nói, biết đọc, còn người mẹ “mồ côi” cả tiếng Việt lẫn tiếng Anh, chỉ bập bõm vài từ để giao tiếp. Đó là hình ảnh sống động của tiệm làm móng nơi người mẹ kiếm ăn độ nhật, nơi bà cả ngày cúi đầu vào chân người khác, nói xin lỗi, nơi ước mơ Mỹ được xây đắp và cũng tan vỡ, nơi bọn trẻ con Mỹ gốc Việt lớn lên, làm bài tập, ăn phở, chơi đùa. Đó là ký ức về cậu bạn trai đầu tiên, Trevor, người Chó Con quen khi đi làm thêm thu hoạch cây thuốc lá ở một trang trại ngoại ô Hartford. Mối tình ấy giờ đây khi nhớ lại khiến lá thư đầy ứ những hoài niệm vừa đẹp đẽ, vừa đau buồn.</p>
+		</div>
+	</div>
+	<footer>
+	<p> QT22TC3.6</p>
+	<p>Copyright Nguyễn Gia Kỳ</p></footer>
+</body>
+</html>
